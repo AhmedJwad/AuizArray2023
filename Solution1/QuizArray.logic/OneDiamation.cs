5b8c7f5f46edcce773ceadf70c0aaa1d4b44de5e
AhmedJwad <Ahmednet380@gmail.com>
2023-12-06 08:17:12 -0800

    QuiZ Array

---
+++ Solution1/QuizArray.logic/OneDiamation.cs
@@ -0,0 +1,39 @@
+﻿namespace QuizArray.logic
+{
+    public class OneDiamation
+    {
+        private int[] _array;
+        private int _top;
+
+        public OneDiamation(int n)
+        {
+            N = n;
+            _array = new int[N];    
+        }
+
+        public int N { get; }
+        private bool IsEmpty => N == 0;
+
+        private bool IsFull => N == _top;
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < _top; i++)
+            {
+                output += $"{_array[i]}\t";
+            }
+            return output;
+        }
+        public void Add(int Value)
+        {
+            if(IsFull)
+            {
+                throw new Exception("The array is full.");
+            }
+            _array[_top] = Value;
+            _top++;
+        }
+
+    }
+}
---
+++ Solution1/QuizArray.logic/TowDiamation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizArray.logic
+{
+    public class TowDiamation
+    {
+        private int[,] _array;
+
+        public TowDiamation(int m, int n)
+        {
+           M= m;
+           N = n;
+            _array = new int[M,N];
+        }
+
+        public int M { get; }
+        public int N { get; }
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    output += $"{_array[i,j]}";
+                }
+            }
+            return output;
+        }
+
+        public OneDiamation ToOneDiamation()
+        {
+            var oneDiamation = new OneDiamation(M * N);
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j <M; j++)
+                {
+                    oneDiamation.Add(_array[i, j]);
+                }
+            }
+            return oneDiamation;
+        }
+
+        public void Fill()
+        {
+            var random = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j= 0; j < N; j++)
+                {
+                    _array[i, j] = random.Next(1, 100);
+                }
+            }
+        }
+    }
+}
